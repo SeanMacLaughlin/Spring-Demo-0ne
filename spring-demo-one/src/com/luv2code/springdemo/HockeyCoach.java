bc95cd1b825b40a382ff50b3cedff31340d8bdc5
geneautry <geneautry@new-host.home>
2019-06-13 22:08:00 -0400

    Updated to unit 36

---
+++ spring-demo-one/src/com/luv2code/springdemo/HockeyCoach.java
@@ -0,0 +1,16 @@
+package com.luv2code.springdemo;
+
+public class HockeyCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Pucks Deep, Ducks Fly Together";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- spring-demo-one/src/com/luv2code/springdemo/SetterDemoApp.java
+++ spring-demo-one/src/com/luv2code/springdemo/SetterDemoApp.java
@@ -2,21 +2,20 @@ package com.luv2code.springdemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class HelloSpringApp {
+public class SetterDemoApp {
 
 	public static void main(String[] args) {
-
+		
 		// load the spring configuration file
-		ClassPathXmlApplicationContext context = 
+		ClassPathXmlApplicationContext context =
 				new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		// retrieve bean from spring container
-		Coach theCoach = context.getBean("myCoach", Coach.class);
+		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 		
-		// call methods on the bean 
+		// call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		
-		// let's call out new method for fortunes
 		System.out.println(theCoach.getDailyFortune());
 		
 		// close the context
